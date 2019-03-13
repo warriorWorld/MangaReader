bf450225b167938ce1d133f420ba3a4e3d428021
suhang <suhang@xianhuahua.com>
2019-03-13 14:48:41 +0800

    gif

--- app/src/main/java/com/truthower/suhang/mangareader/adapter/ReadMangaAdapter.java
+++ app/src/main/java/com/truthower/suhang/mangareader/adapter/ReadMangaAdapter.java
@@ -2,15 +2,20 @@ package com.truthower.suhang.mangareader.adapter;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.truthower.suhang.mangareader.R;
 import com.truthower.suhang.mangareader.config.Configure;
 
 import java.util.ArrayList;
 
+import pl.droidsonroids.gif.GifImageView;
 import uk.co.senab.photoview.PhotoView;
 
 
@@ -49,19 +54,27 @@ public class ReadMangaAdapter extends PagerAdapter {
 
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
-        container.removeView((PhotoView) object);
+        container.removeView((ImageView) object);
     }
 
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
-        PhotoView v0;
-        v0 = new PhotoView(context);
-//        bp = ImageUtil.getImageFromSDFile(pathList.get(position));
-//        v0.setImageBitmap(bp);
-        ImageLoader.getInstance().displayImage(pathList.get(position), v0, Configure.smallImageOptions);
-        container.addView(v0);
-        return v0;
+        if (pathList.get(position).endsWith(".gif") ||
+                pathList.get(position).endsWith(".GIF")) {
+            GifImageView gv = (GifImageView) LayoutInflater.from(context).inflate(R.layout.item_gif, container, false);
+//            ImageLoader.getInstance().displayImage(pathList.get(position), gv, Configure.smallImageOptions);
+            Uri uri= Uri.parse(pathList.get(position));
+            gv.setImageURI(uri);
+            container.addView(gv);
+            return gv;
+        } else {
+            PhotoView v0;
+            v0 = new PhotoView(context);
+            ImageLoader.getInstance().displayImage(pathList.get(position), v0, Configure.smallImageOptions);
+            container.addView(v0);
+            return v0;
+        }
     }
 //根据源码 这个方法可以解决缓存至少是2的问题 但是这个方法不太好 所以我改成用六个缓存view
 //    @Override
