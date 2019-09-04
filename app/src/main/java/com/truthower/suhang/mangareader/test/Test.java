020cfbca3e8c38a2cae526337e326286bcbcd14a
suhang <suhang@xianhuahua.com>
2019-09-04 14:42:07 +0800

    升级Android studio

--- app/src/main/java/com/truthower/suhang/mangareader/test/Test.java
+++ app/src/main/java/com/truthower/suhang/mangareader/test/Test.java
@@ -19,20 +19,33 @@ import java.util.ArrayList;
 public class Test {
     private String[] spiderables = {"http://topwebcomics.com/", "http://mangakakalot.com/"};
     private static org.jsoup.nodes.Document doc;
+    private static int num = 0;
+    private static int addTime = 0;
+    private static int totalAddTime = 0;
 
     public static void main(String[] args) {
-        try {
-            doc = Jsoup.connect("https://www.mangareader.net/one-piece/950")
-                    .timeout(10000).get();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            doc = Jsoup.connect("https://www.mangareader.net/one-piece/950")
+//                    .timeout(10000).get();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        if (null != doc) {
+////            Element mangaPicDetailElement = doc.getElementsByClass("episode-table").first();
+//            Element element = doc.select("div.imgholder").first();
+//            String url = element.getElementsByTag("img").last().attr("src");
+//            System.out.println(url);
+//        }
+        totalAddTime = 100;
+        addTo();
+    }
 
-        if (null != doc) {
-//            Element mangaPicDetailElement = doc.getElementsByClass("episode-table").first();
-            Element element = doc.select("div.imgholder").first();
-            String url = element.getElementsByTag("img").last().attr("src");
-            System.out.println(url);
+    private static void addTo() {
+        if (addTime < totalAddTime) {
+            num = addTime + addTime + 1;
+            addTime++;
+            addTo();
         }
     }
 }
